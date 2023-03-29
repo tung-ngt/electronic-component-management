1a6d8a144beeace241ccb176bc286a23acaab831
Chu Minh <ciltron.com@gmail.com>
2023-03-29 15:08:58 +0700

    rearrange code in pull

--- models/serializers/Pushpulltosql/Pushpulltosql/Pushpulltosql.py
+++ models/serializers/Pushpulltosql/Pushpulltosql/Pushpulltosql.py
@@ -65,7 +65,8 @@ def deserialize(kind:str, myresult:list):
     '''
         Deserialize thing pulled from database
     '''
-    if kind == 'capacitor':
+    compo_list = ['capacitor', 'resister', 'inductor', 'sensor', 'ic']
+    if kind in compo_list:
         mnf_id = myresult[index_compo['mnf_id']]
         price = myresult[index_compo['price']]
         inventory_date = myresult[index_compo['inventory_date']]    
@@ -74,47 +75,17 @@ def deserialize(kind:str, myresult:list):
         special_att = float(myresult[index_compo['special_att']])
         sub_category = myresult[index_compo['sub_category']]
         stock = myresult[index_compo['stock']]
-        return Capacitor(mnf_id, price, inventory_date, guarantee, part_number, sub_category, stock, special_att)
-    elif kind == 'resistor':
-        mnf_id = myresult[index_compo['mnf_id']]
-        price = myresult[index_compo['price']]
-        inventory_date = myresult[index_compo['inventory_date']]    
-        guarantee = myresult[index_compo['guarantee']]
-        part_number = myresult[index_compo['part_number']]
-        special_att = float(myresult[index_compo['special_att']])
-        sub_category = myresult[index_compo['sub_category']]
-        stock = myresult[index_compo['stock']]
-        return Resistor(mnf_id, price, inventory_date, guarantee, part_number, sub_category, stock, special_att)
-    elif kind == 'inductor':
-        mnf_id = myresult[index_compo['mnf_id']]
-        price = myresult[index_compo['price']]
-        inventory_date = myresult[index_compo['inventory_date']]    
-        guarantee = myresult[index_compo['guarantee']]
-        part_number = myresult[index_compo['part_number']]
-        special_att = float(myresult[index_compo['special_att']])
-        sub_category = myresult[index_compo['sub_category']]
-        stock = myresult[index_compo['stock']]
-        return Inductor(mnf_id, price, inventory_date, guarantee, part_number, sub_category, stock, special_att)
-    elif kind == 'sensor':
-        mnf_id = myresult[index_compo['mnf_id']]
-        price = myresult[index_compo['price']]
-        inventory_date = myresult[index_compo['inventory_date']]    
-        guarantee = myresult[index_compo['guarantee']]
-        part_number = myresult[index_compo['part_number']]
-        special_att = myresult[index_compo['special_att']]
-        sub_category = myresult[index_compo['sub_category']]
-        stock = myresult[index_compo['stock']]
-        return Sensor(mnf_id, price, inventory_date, guarantee, part_number, sub_category, stock, special_att)
-    elif kind == 'ic':
-        mnf_id = myresult[index_compo['mnf_id']]
-        price = myresult[index_compo['price']]
-        inventory_date = myresult[index_compo['inventory_date']]    
-        guarantee = myresult[index_compo['guarantee']]
-        part_number = myresult[index_compo['part_number']]
-        special_att = float(myresult[index_compo['special_att']])
-        sub_category = myresult[index_compo['sub_category']]
-        stock = myresult[index_compo['stock']]
-        return IC(mnf_id, price, inventory_date, guarantee, part_number, sub_category, stock, special_att)
+
+        if kind == 'capacitor':
+            return Capacitor(mnf_id, price, inventory_date, guarantee, part_number, sub_category, stock, special_att)
+        elif kind == 'resistor':
+            return Resistor(mnf_id, price, inventory_date, guarantee, part_number, sub_category, stock, special_att)
+        elif kind == 'inductor':
+            return Inductor(mnf_id, price, inventory_date, guarantee, part_number, sub_category, stock, special_att)
+        elif kind == 'sensor':
+            return Sensor(mnf_id, price, inventory_date, guarantee, part_number, sub_category, stock, special_att)
+        elif kind == 'ic':
+            return IC(mnf_id, price, inventory_date, guarantee, part_number, sub_category, stock, special_att)
 
     elif kind == 'manufacturer':
         name = myresult[index_manu['name']]
