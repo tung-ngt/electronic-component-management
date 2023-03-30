bbe9f46b4fcfe4a62c23945d224a1a8093aeebbd
TheUnknovvn <baonpg.bi12-048@st.usth.edu.vn>
2023-03-30 16:00:45 +0700

    edit main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import models.db.create_database as mktable 
+from views.App import EComponentStoreManagementGUI
 
 if __name__ == "__main__":
     # Create db file in "models/db/electronic store.db"
     conn, cur = mktable.get_connection()
     mktable.delete_all_tables(conn)
     mktable.create_tables()
-    
+    app = EComponentStoreManagementGUI()
+    app.mainloop()  
