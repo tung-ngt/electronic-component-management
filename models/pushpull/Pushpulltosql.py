6b8caa92266f75f27c26e4e7bb50768b6ff24fbf
Chu Minh <ciltron.com@gmail.com>
2023-03-30 18:56:59 +0700

    add comment to import section

--- models/pushpull/Pushpulltosql.py
+++ models/pushpull/Pushpulltosql.py
@@ -2,6 +2,7 @@ from os import path
 import sys
 path_to_models = path.abspath(r'C:\Users\ciltr\Desktop\USTH\Semester 2\Python\Python project\electronic-component-management\models')
 sys.path.append(path_to_models)
+#from db.create_database import get_connection, create_tables, delete_all_tables    Run this if you want to use the database using list
 from db.database_with_classes import get_connection, create_tables, delete_all_tables
 from domains import Component
 from serializers.Serializer import serialize, deserialize
