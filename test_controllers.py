a8eb28c2a4deb3efd863212f7286cb6a3d0c635f
Nguyen Thanh Tung <tung.ngt@outlook.com>
2023-04-01 20:58:12 +0700

    test controller and db

--- test_controllers.py
+++ test_controllers.py
@@ -1,6 +1,20 @@
 from controllers.AppController import AppController
 
 
-app_controller = AppController("electronic_store_with_classes.db")
+app_controller = AppController()
 app_controller.load_data_from_db()
-print(app_controller.get_capacitors())
+filters = {
+    "mnf_id": "",
+    "part_number": "MAX",
+    "inventory_date" : [("<=",""), (">=", "")],
+    "price" : [("<=",""), (">=", "")],
+    "guarantee" : [("<=",""), (">=", "")],
+    "sub_category" : [],
+    "stock": [("<=",""), (">=", "")],
+    "clock" : [("<=",""), (">=", "")],
+}
+
+no_result, result = app_controller.get_list_with_filters("ic", filters)
+print(no_result)
+for component in result:
+    print(component.get_all_info())
